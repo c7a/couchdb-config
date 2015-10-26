66d6f0cb2a3f261cd7fed55abfd040d042c48d85
Darcy Quesnel <darcy@c7a.ca>
2015-10-26 20:21:06 +0000

    basic command line and paging support

--- couch-tdrmeta/couch-tdrmeta-latest.pl
+++ couch-tdrmeta/couch-tdrmeta-latest.pl
@@ -2,22 +2,31 @@
 
 use LWP::UserAgent;
 use JSON;
+use Getopt::Long;
+
+my $tdrmeta = 'http://mini.office.c7a.ca:5984/tdrmeta';
+my $skip = 0;
+my $limit = 100;
+GetOptions( 'tdrmeta=s' => \$tdrmeta,
+            'skip=i'    => \$skip,
+            'limit=i'   => \$limit)
+    or die 'Error in command line arguments.';
 
 my $ua = LWP::UserAgent->new;
 
-my $req = HTTP::Request->new(GET =>
-    'http://mini.office.c7a.ca:5984/tdrmeta/_design/attachments/_view/latest' .
-    '?reduce=false&stale=ok');
+my $req = HTTP::Request->new( GET => $tdrmeta . 
+        '/_design/attachments/_view/latest' .
+        "?reduce=false&stale=ok&skip=$skip&limit=$limit" );
 my $res = $ua->request($req);
 
 if ($res->is_success) {
     my $list = from_json($res->content);
 
-    my $nrows = $list->{'total_rows'};
-    foreach my $i (0..$nrows) {
-        $req = HTTP::Request->new(GET =>
-            'http://mini.office.c7a.ca:5984/tdrmeta/' .
-            $list->{'rows'}[$i]->{'id'} . "/" . $list->{'rows'}[$i]->{'value'});
+    foreach my $i (0..$limit-1) {
+        print "$list->{rows}[$i]->{id}\n";
+        $req = HTTP::Request->new( GET => $tdrmeta .
+                '/' . $list->{rows}[$i]->{id} .
+                '/' . $list->{rows}[$i]->{value});
         $res = $ua->request($req);
 
         print $res->content;
