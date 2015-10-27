4583c99f339492e768e2c6fef8455a4804e26062
Darcy Quesnel <darcy@c7a.ca>
2015-10-27 13:47:53 +0000

    basic driver now working

--- couch-tdrmeta/couch-tdrmeta-latest.pl
+++ couch-tdrmeta/couch-tdrmeta-latest.pl
@@ -5,15 +5,16 @@ use JSON;
 use Getopt::Long;
 
 my $tdrmeta = 'http://mini.office.c7a.ca:5984/tdrmeta';
+my $co_search = 'http://mini.office.c7a.ca:5984/co_search';
 my $skip = 0;
 my $limit = 100;
 GetOptions( 'tdrmeta=s' => \$tdrmeta,
-            'skip=i'    => \$skip,
-            'limit=i'   => \$limit)
+            'co_search=s' => \$co_search,
+            'skip=i' => \$skip,
+            'limit=i' => \$limit )
     or die 'Error in command line arguments.';
 
 my $ua = LWP::UserAgent->new;
-
 my $req = HTTP::Request->new( GET => $tdrmeta . 
         '/_design/attachments/_view/latest' .
         "?reduce=false&stale=ok&skip=$skip&limit=$limit" );
@@ -23,14 +24,20 @@ if ($res->is_success) {
     my $list = from_json($res->content);
 
     foreach my $i (0..$limit-1) {
-        print "$list->{rows}[$i]->{id}\n";
         $req = HTTP::Request->new( GET => $tdrmeta .
                 '/' . $list->{rows}[$i]->{id} .
-                '/' . $list->{rows}[$i]->{value});
+                '/' . $list->{rows}[$i]->{value} );
         $res = $ua->request($req);
 
-        print $res->content;
+        # TODO: do stuff with $res-content (XML) return $id and $content (JSON)
+        my $id = 'aaa';
+        my $content = '{"foo":"bar"}';
 
+        $req = HTTP::Request->new(PUT => $co_search . '/' . $id);
+        $req->header('Content-Type' => 'application/json');
+        $req->content($content);
+        $res = $ua->request($req);
+        print $res->status_line, "\n";
     }
 
 } else {
