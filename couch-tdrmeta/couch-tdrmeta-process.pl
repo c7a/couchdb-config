54f76b45b747800c1bb692752eb1115da4d0b848
Darcy Quesnel <darcy@c7a.ca>
2015-10-30 20:00:24 +0000

    some cleanup and fiddling around with update - not in a good state yet

--- couch-tdrmeta/couch-tdrmeta-process.pl
+++ couch-tdrmeta/couch-tdrmeta-process.pl
@@ -3,7 +3,9 @@
 use strict;
 use warnings;
 
-# TODO: POD
+# TODO:
+# Add POD
+# Add processed to tdrmeta
 
 use LWP::UserAgent;
 use Getopt::Long;
@@ -13,27 +15,33 @@ use CARP;
 my $tdrmeta = 'http://mini.office.c7a.ca:5984/tdrmeta';
 my $co_search = 'http://mini.office.c7a.ca:5984/co_search';
 my $skip = 0;
-my $limit = 100;
+my $limit = 0;
 GetOptions( 'tdrmeta=s' => \$tdrmeta,
-            'co_search=s' => \$co_search,
-            'skip=i' => \$skip,
-            'limit=i' => \$limit )
+        'co_search=s' => \$co_search,
+        'skip=i' => \$skip,
+        'limit=i' => \$limit )
     or croak 'Error in command line arguments.';
 
-my $ua = LWP::UserAgent->new;
+my $ua = LWP::UserAgent->new(timeout => 8*60);
+
+# GET a list of the latest attachments from tdrmeta
 my $req = HTTP::Request->new( GET => $tdrmeta . 
-            '/_design/attachments/_view/latest' .
-            "?reduce=false&stale=ok&skip=$skip&limit=$limit" );
+        '/_design/attachments/_view/latest' .
+        '?reduce=false' .
+        '&stale=ok' .
+        ($skip ? "&skip=$skip" : '') .
+        ($limit ? "&limit=$limit" : '') );
 my $res = $ua->request($req);
 if ($res->is_success) {
 
     my $list = from_json($res->content);
 
-    foreach my $i ($skip .. $skip + $limit - 1) {
+    foreach my $i ($skip .. $skip + ($limit ? $limit : $list->{total_rows}) - 1) {
 
+        # GET the latest metadata.xml attachment from tdrmeta
         $req = HTTP::Request->new( GET => $tdrmeta .
-                    '/' . $list->{rows}[$i]->{id} .
-                    '/' . $list->{rows}[$i]->{value} );
+                '/' . $list->{rows}[$i]->{id} .
+                '/' . $list->{rows}[$i]->{value} );
         $res = $ua->request($req);
         if ($res->is_success) {
 
@@ -43,12 +51,13 @@ if ($res->is_success) {
             my $id = 'aaa';
             my $content = '{"foo":"bar"}';
 
+            # PUT the processed JSON document in co_search
             $req = HTTP::Request->new(PUT => $co_search . '/' . $id);
             $req->header('Content-Type' => 'application/json');
             $req->content($content);
             $res = $ua->request($req);
     
-            print $res->status_line, "\n"; # Just for feedback.
+            print $res->status_line, "\n"; # Feedback
 
         } else {
             print $res->status_line, "\n"; # Error
--- couch-tdrmeta/couch-tdrmeta-update.pl
+++ couch-tdrmeta/couch-tdrmeta-update.pl
@@ -7,39 +7,71 @@ use warnings;
 # Create new docs
 # Update publicReplicas
 # Update updated
-# POD 
+# Add POD 
 
 use LWP::UserAgent;
 use Getopt::Long;
+use DateTime;
 use JSON;
 use Carp;
 
 my $tdrmeta = 'http://mini.office.c7a.ca:5984/tdrmeta';
 my $tdrepo = 'http://beemster.office.c7a.ca:5984/tdrepo';
 my $skip = 0;
-my $limit = 100;
+my $limit = 0;
 GetOptions( 'tdrmeta=s' => \$tdrmeta,
-            'tdrepo=s' => \$tdrepo,
-            'skip=i' => \$skip,
-            'limit=i' => \$limit )
+        'tdrepo=s' => \$tdrepo,
+        'skip=i' => \$skip,
+        'limit=i' => \$limit )
     or croak 'Error in command line arguments.';
 
-my $ua = LWP::UserAgent->new;
-my $req = HTTP::Request->new( GET => $tdrepo . 
-            '/_design/tdr/_view/newestaip' .
-            "?group_level=1&stale=ok&skip=$skip&limit=$limit" );
-my $res = $ua->request($req);
+my $ua = LWP::UserAgent->new(timeout => 8*60);
 
+# GET a list of the AIP locations from tdrep
+my $req = HTTP::Request->new( GET => $tdrepo . 
+        '/_design/tdr/_view/newestaip' .
+        '?group_level=1' .
+        '&stale=ok' .
+        ($skip ? "&skip=$skip" : '') .
+        ($limit ? "&limit=$limit" : '') );
+my $res = $ua->request($req);
 if ($res->is_success) {
 
     my $list = from_json($res->content);
 
-    foreach my $i (0 .. $limit - 1) {
-        print to_json( { id => $list->{rows}[$i]->{key},
-                updated => $list->{rows}[$i]->{value}[0],
-                publicReplicas => $list->{rows}[$i]->{value}[1] } ), "\n";
-    }
+    foreach my $i (0 .. scalar @{$list->{rows}} - 1) {
 
+        #print $list->{rows}[$i]->{key} . "\n";
+        #print join(', ', @{$list->{rows}[$i]->{value}}) . "\n";
+        #print join(', ', @{$list->{rows}[$i]->{value}[1]}) . "\n";
+
+        # GET a document from tdrmeta
+        $req = HTTP::Request->new( GET => $tdrmeta .
+                '/' . $list->{rows}[$i]->{key} );
+        $res = $ua->request($req);
+        if ($res->is_success) {
+
+            my $content = from_json($res->content);
+
+            my ($s,$m,$h,$d,$o,$y) = (localtime)[0,1,2,3,4,5];
+            print to_json( { _id => $content->{_id},
+                    _rev => $content->{_rev},
+                    updated => ($y+1900).'-'.($o+1).'-'.$d,
+                    publicReplicas => $list->{rows}[$i]->{value}[1] } ), "\n";
+ 
+            # PUT the updated document in tdrmeta
+            #$req = HTTP::Request->new( GET => $tdrmeta .
+            #'/' . $list->{rows}[$i]->{key} );
+            #$req->header('Content-Type' => 'application/json');
+            #$req->content($content);
+            #$res = $ua->request($req);
+    
+            print $res->status_line, "\n"; # Feedback
+ 
+        } else {
+            print $res->status_line, "\n"; # Error
+        }
+    }
 } else {
     print $res->status_line, "\n"; # Error
 }
