d6fd0b1d11d68ecbbb40a11d98dbd6c1602079e1
Russell McOrmond <russell.mcormond@canadiana.ca>
2015-12-08 20:52:38 +0000

    Updates to basic _update to have "nocreate" flag, and to only update fields if value changed.  Started on 'public_repo' field which, being an array, isn't a simple scalar comparison #4570

--- couch-internalmeta/design/updates/basic.js
+++ couch-internalmeta/design/updates/basic.js
@@ -3,35 +3,53 @@ function(doc, req){
     // Javascript toISOString() includes parts of a second, which we strip.
     var nowdates = nowdate.toISOString().replace(/\..*Z/,"Z");
     var updated=false;
-    if (!doc) {
-        if ('id' in req && req['id']) {
-            // create new document
-            doc = {};
-            doc['_id'] = req['id'];
-            doc['created'] = nowdates;
-            updated=true;
-        } else {
-          // change nothing in database
-          return [null, '{"error": "Missing ID"}\n']
-        }
-    }
     if ('form' in req) {
         var updatedoc = req.form;
+
+        if (!doc) {
+            if ('id' in req && req['id']) {
+                if ('nocreate' in updatedoc) {
+                    return [null, '{"return": "no create"}\n'];
+                } else {
+                    // create new document
+                    doc = {};
+                    doc['_id'] = req['id'];
+                    doc['created'] = nowdates;
+                    updated=true;
+                }
+            } else {
+                // change nothing in database
+                return [null, '{"error": "Missing ID"}\n']
+            }
+        }
         if ('public_repo' in updatedoc) {
             // This parameter sent as JSON encoded string
-            doc['public_repo'] = JSON.parse(updatedoc['public_repo']);
-            doc['type'] = 'aip';
-            updated=true;
+            var pubrepo = JSON.parse(updatedoc['public_repo']);
+
+            // Equality is same membership, even if different order
+            function hasSameMembers(repo1,repo2) {
+                if (!repo1 || !repo2 || repo1.length != repo2.length) {
+                    return false;
+                }
+                // cheating for now
+                return true;
+            }
+
+            if (!hasSameMembers(doc['public_repo'],pubrepo)) {
+                doc['public_repo'] = pubrepo;
+                doc['type'] = 'aip';
+                updated=true;
+            }
         }
-        if ('type' in updatedoc) {
+        if (('type' in updatedoc) && (doc['type']!== updatedoc['type'])) {
             doc['type'] = updatedoc['type'];
             updated=true;
         }
-        if ('sub-type' in updatedoc) {
+        if (('sub-type' in updatedoc) && (doc['sub-type'] !== updatedoc['sub-type'])) {
             doc['sub-type'] = updatedoc['sub-type'];
             updated=true;
         }
-        if ('approved' in updatedoc) {
+        if (('approved' in updatedoc) && (doc['approved'] !== updatedoc['approved'])) {
             doc['approved'] = updatedoc['approved'];
             updated=true;
         }
