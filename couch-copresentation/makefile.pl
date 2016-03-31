c5070afacec7ebd84e4c51c2a174c8dc8a06a5df
Russell McOrmond <russell.mcormond@canadiana.ca>
2016-03-31 20:44:59 +0000

    Basic loop for cmr.json data to push to individual cosearch and copresentation documents #4703

---
+++ couch-copresentation/makefile.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use strict;
+use Carp;
+use warnings;
+use Config::General;
+
+sub readjs {
+    my $filename = shift(@_);
+    open FILE, $filename or die "Couldn't open $filename: $!"; 
+    my $jsstring = join("", <FILE>); 
+    close FILE;
+    $jsstring =~ s/[\\]/\\\\/g;
+    $jsstring =~ s/[\n]/\\n/g;
+    $jsstring =~ s/[\"]/\\"/g;
+    return $jsstring;
+}
+
+my $basicupdate = readjs("design/updates/basic.js");
+
+open FILE, ">tdr.js" or die "Couldn't open tdr.js: $!";
+print FILE <<EOF;
+exports.views = {
+}
+exports.lists = {
+}
+exports.updates = {
+     "basic": "${basicupdate}"
+}
+
+EOF
+close FILE;
+
+my $arg = shift;
+if ($arg && $arg eq "kanso") {
+    my $conf = new Config::General (
+        -ConfigFile => "/etc/canadiana/tdr/tdr.conf"
+        );
+    my %confighash = $conf->getall;
+    if (exists $confighash{copresentation}) {
+        my $dburl = $confighash{copresentation}{server}."/".$confighash{copresentation}{database}."/";
+        print "Pushing to $dburl\n";
+        `/usr/bin/kanso push $dburl`;
+    } else {
+        croak "Missing <copresentation> configuration block in config\n";
+    }
+}
