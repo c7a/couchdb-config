a53a1109defcd5501ecd12c4c21c42baa0a411b2
Russell McOrmond <russell.mcormond@canadiana.ca>
2016-06-01 15:22:11 +0000

    _view also emits boolean indicating if issue is approved #4880

--- couch-internalmeta/design/views/issues.map.js
+++ couch-internalmeta/design/views/issues.map.js
@@ -11,5 +11,5 @@ function(doc) {
     if (seq<=9999) {
         seq = ("000"+seq).slice(-4);
     }
-    emit([doc.parent,seq], { label: doc['label'] , pubmin: doc['pubmin'] } );
+    emit([doc.parent,seq], { label: doc['label'] , pubmin: doc['pubmin'], approved: 'approved' in doc } );
 }
