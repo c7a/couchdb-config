86357245deaf53d875ebe560bde57a673fa5f2cb
Russell McOrmond <russell.mcormond@canadiana.ca>
2016-08-30 20:17:29 +0000

    Adding date check to view (Req kept in document for now), fixed issues with basic wrapper. #4974

--- couch-wipmeta/design/views/ingestq.map.js
+++ couch-wipmeta/design/views/ingestq.map.js
@@ -2,5 +2,10 @@ function(doc) {
     if (!('ingestReq' in doc) || 'processdate' in doc.ingestReq ) {
         return;
     }
+    if('ingestHistory' in doc && 
+       Array.isArray(doc['ingestHistory']) &&
+       doc.ingestHistory[0].date > doc.ingestReq.date ) {
+        return;
+    }
     emit(doc.ingestReq.date,null);
 }
