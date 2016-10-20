c46604bc81da169758f2977d54e8c320227220ba
Russell McOrmond <russell.mcormond@canadiana.ca>
2016-10-20 16:43:14 +0000

    Update to support generic processing request fields #5007

--- couch-wipmeta/design/updates/basic.js
+++ couch-wipmeta/design/updates/basic.js
@@ -95,6 +95,78 @@ function(doc, req){
                 return [null, '{"return": "no exportReq"}\n']
             }
         }
+
+        // Generalized process request related fields
+        if ('processreq' in updatedoc) {
+            var processReq = JSON.parse(updatedoc.processreq);
+            if (!('date' in processReq)) {
+                processReq.date = nowdates;
+            }
+            if(!'processReq' in doc || 
+               !Array.isArray(doc.processReq)) {
+                doc.processReq=[];
+            }
+            doc.processReq.unshift(processReq);
+            updated=true;
+        }
+        if ('processing' in updatedoc) {
+            var ph = JSON.parse(updatedoc.processing);
+            if (!('date' in ph)) {
+                ph.date = nowdates;
+            }
+            if(!'processReq' in doc || 
+               !Array.isArray(doc.processReq)) {
+                return [null, '{"return": "no processReq"}\n']
+            }
+            if (!('reqdate' in ph) || 
+                ph.reqdate !== doc.processReq[0].date) {
+                return [null, '{"return": "No matching request date"}\n']
+            }
+            if (!('request' in ph) || 
+                ph.request !== doc.processReq[0].request) {
+                return [null, '{"return": "No matching request type"}\n']
+            }
+            if ('processhost' in doc.processReq[0]) {
+                if (doc.processReq[0].processhost === ph.host) {
+                    return [null, '{"return": "already set"}\n'];
+                } else {
+                    return [null, '{"return": "other host"}\n'];
+                }
+            } else {
+                doc.processReq[0].processhost = ph.host;
+                doc.processReq[0].processdate = ph.date;
+                updated=true;
+            }
+        }
+        if ('processed' in updatedoc) {
+            var processed = JSON.parse(updatedoc['processed']);
+            if (!('date' in processed)) {
+                processed.date = nowdates;
+            }
+            if(!'processHistory' in doc || 
+               !Array.isArray(doc.processHistory)) {
+                doc.processHistory=[];
+            }
+            if(!'processReq' in doc || 
+               !Array.isArray(doc.processReq)) {
+                return [null, '{"return": "no processReq"}\n']
+            }
+            if (!('reqdate' in processed) || 
+                processed.reqdate !== doc.processReq[0].date) {
+                return [null, '{"return": "No matching request date"}\n']
+            }
+            if (!('request' in processed) || 
+                processed.request !== doc.processReq[0].request) {
+                return [null, '{"return": "No matching request type"}\n']
+            }
+            processed.req=doc.processReq.shift();
+            doc.processHistory.unshift(processed);
+            updated=true;
+        }
+
+
+
+        // Manipulating Label (METS fragment)
         if ('label' in updatedoc) {
             doc.label=updatedoc.label;
             updated=true;
