5e044dabbe8ac07a034e170a85de54a0d028e015
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-02-27 15:10:58 +0000

    Don't report on silenced AIPs.

--- couch-wipmeta/design/views/processs.map.js
+++ couch-wipmeta/design/views/processs.map.js
@@ -3,6 +3,9 @@ function(doc) {
         return;
     };
     req=doc.processHistory[0];
+    if (req.request === 'silence') {
+        return;
+    }
     datep=req.date.split("T");
     emit ([req.status,req.message !== "",datep[0],datep[1]],req.request);
 }
