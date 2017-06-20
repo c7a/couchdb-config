e7a22ce4ad7a00fbc1e6b1a31838f0a904561a60
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-06-20 18:26:04 +0000

    Handle situation where the order field is a 0-length array.

--- couch-copresentation/design/views/borndigital.map.js
+++ couch-copresentation/design/views/borndigital.map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if ('components' in doc && 'order' in doc && Array.isArray(doc.order)) {
+    if ('components' in doc && 'order' in doc && Array.isArray(doc.order) && doc.order.length >0) {
         var acomponent=doc.components[doc.order[0]];
         if(!("canonicalMaster" in acomponent) || acomponent.canonicalMaster === null) {
             emit(doc['_id'].split('.')[0],null);
